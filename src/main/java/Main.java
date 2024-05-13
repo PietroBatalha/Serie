1852a576e9d74981b1e03ebe6d39c9f0d7f6aefd
PietroBatalha <169068274+PietroBatalha@users.noreply.github.com>
2024-05-13 21:17:56 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+ex
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("#####");
+    System.out.println("Série");
+    System.out.println("#####");
+    System.out.println("");
+ System.out.println("############");
+    System.out.println("Breaking Bad");
+    System.out.println("############");
+    System.out.println(" ");
+    System.out.println("Breaking Bad  é uma série de televisão americana que acompanha a transformação de um professor de química, Walter White, em um poderoso traficante de drogas após ser diagnosticado com câncer terminal. Com a ajuda de um ex-aluno, Jesse Pinkman, Walter mergulha no mundo perigoso do tráfico, enfrentando dilemas morais e desafios cada vez maiores enquanto tenta garantir o futuro financeiro de sua família. A série explora temas de moralidade, consequências das escolhas e até que ponto alguém pode ir para proteger aqueles que ama.");
+
+    System.out.println("############");
+    System.out.println("foi lançado em 20 de janeiro de 2008. A série foi ao ar pela primeira vez no canal de televisão AMC nos Estados Unidos e teve um total de cinco temporadas, encerrando em 29 de setembro de ");
+  
+  
+    
+  }
+  
+
+}
